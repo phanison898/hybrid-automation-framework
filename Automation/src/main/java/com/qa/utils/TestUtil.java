9d4fb7821b8116c147a269675a75207e7bf1df6d
phani kumar <phani kumar@LAPTOP-TBE98ORM>
2020-01-27 02:34:11 +0530

    added a method in TestUtil.class for generating html file from log file

--- Automation/src/main/java/com/qa/utils/TestUtil.java
+++ Automation/src/main/java/com/qa/utils/TestUtil.java
@@ -1,6 +1,10 @@
 package com.qa.utils;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -39,6 +43,32 @@ public class TestUtil extends Base{
 		}catch(Exception e) {
 			
 		}
+	}
+	
+	public static void generateHtmlFile(String src, String dest) {
+		BufferedReader bf = null;
+		try {
+			bf = new BufferedReader(new FileReader(src));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		String str = "";
+		String s;
+		try {
+			while((s = bf.readLine())!=null) {
+				str += s;
+				str += "<br />";
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		String html = "<!DOCTYPE html><html><head></head><body>"+str+"</body></html>";
+		FileWriter writter = null;
+		try {
+			writter = new FileWriter(new File(dest));
+			writter.write(html);
+			writter.close();
+		} catch (IOException e) {e.printStackTrace();}
 	}
 
 }
