d1d9673247a5354e7d65b00f5162f6a63b3e4c66
phanison898 <phanison898@gmail.com>
2020-01-27 20:04:42 +0530

    Added event firing driver script in Base

--- Automation/src/main/java/com/qa/base/Base.java
+++ Automation/src/main/java/com/qa/base/Base.java
@@ -1,8 +1,5 @@
 package com.qa.base;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.logging.log4j.LogManager;
@@ -13,65 +10,87 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
+import org.openqa.selenium.support.events.WebDriverEventListener;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+
+import com.qa.reporting.WebDriverEvents;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class Base extends Configuration{
-	
-	static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+public class Base extends Configuration {
+
+	private static WebDriver e_driver;
 	static long PAGELOAD_TIME = 60;
 	static long IMPLICIT_TIME = 5;
 	static long SCRIPTLOAD_TIME = 5;
-	static Logger logger = LogManager.getLogger(Base.class);
+	private static Logger logger = LogManager.getLogger(Base.class);
+	public static EventFiringWebDriver driver;
+	private static WebDriverEventListener eventListener;
 	
 	public Base() {
 		
+		
+
 	}
-	
-	@BeforeMethod public void start() {
+
+	@BeforeMethod
+	public void start() {
 		initBrowser(config("browser"));
 		System.out.println(config("reportName"));
+		logger.info("this is a log");
 	}
-	
-	@AfterMethod public void end() {
-		driver().quit();
+
+	@AfterMethod
+	public void end() {
+		driver.quit();
 	}
-	
-	public static WebDriver driver() {
-		return driver.get();
-	}
+
 	
 	public static void initBrowser(String browserName) {
-		switch(browserName) {
+		switch (browserName) {
 		case BrowserType.CHROME:
 			WebDriverManager.chromedriver().setup();
-			driver.set(new ChromeDriver());
+			e_driver = new ChromeDriver();
+			driver = new EventFiringWebDriver(e_driver);
+			eventListener = new WebDriverEvents();
+			driver.register(eventListener);
 			break;
 		case BrowserType.FIREFOX:
-			WebDriverManager.firefoxdriver().setup();
-			driver.set(new FirefoxDriver());
+			WebDriverManager.chromedriver().setup();
+			e_driver = new ChromeDriver();
+			driver = new EventFiringWebDriver(e_driver);
+			eventListener = new WebDriverEvents();
+			driver.register(eventListener);
 			break;
 		case BrowserType.EDGE:
-			WebDriverManager.edgedriver().setup();
-			driver.set(new EdgeDriver());
+			WebDriverManager.chromedriver().setup();
+			e_driver = new ChromeDriver();
+			driver = new EventFiringWebDriver(e_driver);
+			eventListener = new WebDriverEvents();
+			driver.register(eventListener);
 			break;
 		case BrowserType.IE:
-			WebDriverManager.iedriver().setup();
-			driver.set(new InternetExplorerDriver());
+			WebDriverManager.chromedriver().setup();
+			e_driver = new ChromeDriver();
+			driver = new EventFiringWebDriver(e_driver);
+			eventListener = new WebDriverEvents();
+			driver.register(eventListener);
 			break;
 		default:
 			WebDriverManager.chromedriver().setup();
-			driver.set(new ChromeDriver());
+			e_driver = new ChromeDriver();
+			driver = new EventFiringWebDriver(e_driver);
+			eventListener = new WebDriverEvents();
+			driver.register(eventListener);
 			break;
 		}
-		driver().manage().window().maximize();
-		driver().manage().deleteAllCookies();
-		driver().manage().timeouts().pageLoadTimeout(PAGELOAD_TIME, TimeUnit.SECONDS);
-		driver().manage().timeouts().implicitlyWait(IMPLICIT_TIME, TimeUnit.SECONDS);
-		driver().manage().timeouts().setScriptTimeout(SCRIPTLOAD_TIME, TimeUnit.SECONDS);
-		driver().navigate().to(config("url"));
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(PAGELOAD_TIME, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(IMPLICIT_TIME, TimeUnit.SECONDS);
+		driver.manage().timeouts().setScriptTimeout(SCRIPTLOAD_TIME, TimeUnit.SECONDS);
+		driver.navigate().to(config("url"));
 	}
 }
