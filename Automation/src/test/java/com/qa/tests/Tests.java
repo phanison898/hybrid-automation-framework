5eedee13a2b5b1f984344ed950507263a7580f59
phanison898 <phanison898@gmail.com>
2020-01-27 20:06:49 +0530

    minor changes

--- Automation/src/test/java/com/qa/tests/Tests.java
+++ Automation/src/test/java/com/qa/tests/Tests.java
@@ -1,23 +1,22 @@
 package com.qa.tests;
 
-import org.testng.Assert;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 import com.qa.utils.TestUtil;
 
 public class Tests extends TestUtil{
-	
-	
+	static Logger logger = LogManager.getLogger(Tests.class);
+
 	@Test
 	public void test1() {
-		Assert.assertTrue(true);
-		log("test case passed");
+		WebElement el =driver.findElement(By.xpath("//input[@title='Search']"));
+		el.clear();
+		el.sendKeys("java latest version");
+		el.submit();
 	}
 	
-	@Test
-	public void test2() {
-		Assert.assertTrue(false);
-		log("test case failed");
-	}
-
 }
